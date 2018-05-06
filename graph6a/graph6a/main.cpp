11b7f9c662ec2ae3defb35d776523a18c62d4da0
Alana Martin <alana@stonybrookweb.com>
2018-05-06 14:42:35 -0400

    Assigned source code

---
+++ graph6a/graph6a/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  graph6a
+//
+//  Created by Alana Martin on 5/6/18.
+//  Copyright © 2018 Alana Martin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
